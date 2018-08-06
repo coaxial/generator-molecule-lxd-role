aa5426930a7a70687c14190b439e8ad345114203
Pierre <coaxial@users.noreply.github.com>
2018-08-06 02:23:59 +0000

    Allow unprivileged nesting (#20)

--- generators/app/templates/setup.sh
+++ generators/app/templates/setup.sh
@@ -10,3 +10,11 @@ sudo lxd init --auto
 
 pip install ansible
 pip install molecule
+
+# Enable nesting without privileged containers,
+# cf. https://stgraber.org/2017/06/15/custom-user-mappings-in-lxd-containers/
+sudo sed -i 's/^\(lxd:[0-9]\{1,\}\):.*/\1:1000000000/' /etc/subuid
+sudo sed -i 's/^\(root:[0-9]\{1,\}\):.*/\1:1000000000/' /etc/subuid
+sudo sed -i 's/^\(lxd:[0-9]\{1,\}\):.*/\1:1000000000/' /etc/subgid
+sudo sed -i 's/^\(root:[0-9]\{1,\}\):.*/\1:1000000000/' /etc/subgid
+sudo systemctl restart lxd
