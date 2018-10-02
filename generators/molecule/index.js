16ba944b77f202d203b2fdd1a546c2dadb261339
coaxial <hi@64b.it>
2018-10-02 19:30:45 -0400

    Add molecule subgenerator skel

---
+++ generators/molecule/index.js
@@ -0,0 +1,37 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+
+module.exports = class extends Generator {
+  prompting() {
+    // Have Yeoman greet the user.
+    this.log(
+      `Welcome to the fine ${chalk.red('generator-molecule-lxd-role')} generator!`,
+    );
+
+    const prompts = [
+      {
+        type: 'confirm',
+        name: 'someAnswer',
+        message: 'Would you like to enable this option?',
+        default: true,
+      },
+    ];
+
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    });
+  }
+
+  writing() {
+    this.fs.copy(
+      this.templatePath('dummyfile.txt'),
+      this.destinationPath('dummyfile.txt'),
+    );
+  }
+
+  install() {
+    this.installDependencies();
+  }
+};
